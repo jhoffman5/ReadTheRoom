8fff3aba5f416306bef0dc8dfcd0d0470098f4b2
Hoffman-Joshua <jhoffman5@carthage.edu>
2019-05-08 15:50:38 -0500

    created simple socket structure and chat.html

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const socket = require('socket.io');
 const session = require('express-session');
 
 const DBAbstraction = require('./DBAbstraction');
 const db = new DBAbstraction('mongodb://localhost:27017');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,6 +19,25 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.use(session({ secret: 'keyboard-cat', cookie: { maxAge: 600000 } }));
+
+var numUsers = 0;
+
+app.get('/', (req, res) => {
+    res.render('chat')
+})
+
+io.on('connection', (socket) => {
+    var addedUser = false;
+
+    console.log('New user connected')
+
+    socket.on('new message', (data) => {
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message, data
+        });
+    });
+});
 
 app.use((req, res) => {
     res.status(404).send(`<h2>Oopsie daisy!</h2><p>Sorry ${req.url} cannot be found.</p>`);
