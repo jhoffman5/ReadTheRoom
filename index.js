76bc687cfb0dd49b66a9535551a0217f93bc612c
Adam Curtis <acurtis@carthage.edu>
2019-05-10 14:58:50 -0500

    Created simple chat functionality

--- index.js
+++ index.js
@@ -9,33 +9,31 @@ const DBAbstraction = require('./DBAbstraction');
 const db = new DBAbstraction('mongodb://localhost:27017');
 
 const app = express();
-const server = require('http').Server(app);
+
+const server = app.listen(42069, function() {
+    console.log("Listening at port 42069.");
+});
+
+app.use(express.static('public'));
+
 const io = require('socket.io')(server);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static('public'));
-
-app.use(session({ secret: 'keyboard-cat', cookie: { maxAge: 600000 } }));
+//app.use(session({ secret: 'keyboard-cat', cookie: { maxAge: 600000 } }));
 
 var numUsers = 0;
 
 app.get('/', (req, res) => {
-    res.render('chat')
+    res.render('index')
 })
 
 io.on('connection', (socket) => {
-    var addedUser = false;
-
-    console.log('New user connected')
-
-    socket.on('new message', (data) => {
-        socket.broadcast.emit('new message', {
-            username: socket.username,
-            message, data
-        });
+    console.log('New user connected');
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
     });
 });
 
@@ -43,11 +41,13 @@ app.use((req, res) => {
     res.status(404).send(`<h2>Oopsie daisy!</h2><p>Sorry ${req.url} cannot be found.</p>`);
 });
 
-db.init()
-    .then(() => {
-        app.listen(42069, () => console.log(`The server is up and running...`));
-    })
-    .catch(err => {
-        console.log("Problem setting up the database.");
-        console.log(err);
-    });
+
+
+// db.init()
+//     .then(() => {
+//         app.listen(42069, () => console.log(`The server is up and running...`));
+//     })
+//     .catch(err => {
+//         console.log("Problem setting up the database.");
+//         console.log(err);
+//     });
---
+++ public/connectUser.js
@@ -0,0 +1,17 @@
+var socket = io.connect('http://localhost:42069');
+
+var message = document.getElementById('message');
+var username = document.getElementById('username');
+var button = document.getElementById('sendMessage');
+var output = document.getElementById('output');
+
+button.addEventListener('click', function(){
+    socket.emit('chat', {
+        message: message.value,
+        username: username.value,
+    });
+});
+
+socket.on('chat', function(data){
+    output.innerHTML += '<p><strong>' + data.username + ':</strong>' + data.message + "</p";
+})
